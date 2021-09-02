5cdb6e46d3ae1f194d6b170c569843615011b911
oluwatobi <adekomiadeleke@gmail.com>
2021-09-02 03:38:34 +0100

    'update3'

--- comm/settings.py
+++ comm/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'comm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
 
 # Password validation
